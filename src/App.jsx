040251eed1fc01b07453f7853d34789404f1c003
Moazzmedhat <moazmedhat028@gmail.com>
2025-04-16 18:34:23 +0200

    Fix GitHub Pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import MainPage from './components/MainPage';
 import ArchivePage from './components/ArchivePage';
 import CompletedTasksPage from './components/CompletedTasksPage';
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-   base: '/todo_list/',
   plugins: [react()],
+  base: '/todo_list/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false
+  }
 })
